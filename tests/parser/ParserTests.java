c41689f23b164af733425858a0b4a9af680ce427
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-24 18:19:59 -0500

    Added JUnit for Unit Testing

---
+++ tests/parser/ParserTests.java
@@ -0,0 +1,14 @@
+package parser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParserTests {
+    @Test
+    public void test() {
+        int i = 0;
+
+        assertEquals(i, 0);
+    }
+}
