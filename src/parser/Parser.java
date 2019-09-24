91578f44d4967278f9fbdcfed190b950f994c80b
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-24 17:33:31 -0500

    Added gen/ folder as a Generated Source Folder so IntelliJ would compile it

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,4 +1,10 @@
 package parser;
 
+import org.antlr.v4.runtime.CharStreams;
+import parser.antlr.testLexer;
+
 public class Parser {
+    public static void main(String[] args) { // Temp main function
+        testLexer lexer = new testLexer(CharStreams.fromString("INSERT INTO regex VALUES FROM (regex, regex);"));
+    }
 }
