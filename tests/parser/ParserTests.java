92d4f66e43ecaa74d4b35a5ca8180cf4e1015852
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-27 17:41:37 -0500

    Added basic tests for type identifier

--- tests/parser/ParserTests.java
+++ tests/parser/ParserTests.java
@@ -1,14 +1,45 @@
 package parser;
 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.Test;
+import parser.antlr.SQLGrammarLexer;
+import parser.antlr.SQLGrammarParser;
 
 import static org.junit.Assert.*;
 
 public class ParserTests {
-    @Test
-    public void test() {
-        int i = 0;
+    public SQLGrammarParser getParserFromText(String text) {
+        SQLGrammarLexer lexer = new SQLGrammarLexer(CharStreams.fromString(text));
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
 
-        assertEquals(i, 0);
+        return new SQLGrammarParser(tokenStream);
+    }
+
+    @Test // These tests are pretty useless, as the ANTLR errors aren't thrown like actual errors
+    public void identifier_shouldAllow() {
+        String[] inputs ={ "a", "csce315", "h_ello", "_variable" };
+
+        for(String in : inputs) {
+            System.out.print(in + " ");
+            SQLGrammarParser parser = getParserFromText(in);
+
+            String actual = parser.identifier().getText();
+
+            assertEquals(in, actual);
+        }
+    }
+
+    @Test // Checks to see if the VARCHAR gets the correct size of it
+    public void type_varchar_getsCorrectSize() {
+        String input = "VARCHAR(20)";
+        String expected = "20";
+
+        SQLGrammarParser parser = getParserFromText(input);
+
+        String actual = parser.type().integer().getText();
+
+        assertEquals(expected, actual);
     }
 }
