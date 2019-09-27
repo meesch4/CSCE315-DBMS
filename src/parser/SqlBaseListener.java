ccf5b39acd5055db83003c500c4b627971261b1c
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-27 17:41:15 -0500

    Updated SqlBaseListener and Main class with basic outline

--- src/parser/SqlBaseListener.java
+++ src/parser/SqlBaseListener.java
@@ -1,10 +1,13 @@
 package parser;
 
 import parser.antlr.*;
+import dbms.*;
 
 // From following the "Guide - ANTLR4 Listener Setup" here: https://docs.google.com/document/d/1kCv5ODfAh8HZZ8uugMLcqMqJLvs9RQDmhvBGt3l2Fzo/edit
 public class SqlBaseListener extends SQLGrammarBaseListener {
-    public SqlBaseListener() {
+    private Dbms dbms;
 
+    public SqlBaseListener() {
+        this.dbms = new Dbms();
     }
 }
