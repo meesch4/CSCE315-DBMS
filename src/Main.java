596e325e6c364530e68fb18b891911697fe7f4bb
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-27 16:57:28 -0500

    Added Main & SqlBaseListener classes

---
+++ src/Main.java
@@ -0,0 +1,4 @@
+
+// Main function, where we do everything
+public class Main {
+}
---
+++ src/parser/SqlBaseListener.java
@@ -0,0 +1,10 @@
+package parser;
+
+import parser.antlr.*;
+
+// From following the "Guide - ANTLR4 Listener Setup" here: https://docs.google.com/document/d/1kCv5ODfAh8HZZ8uugMLcqMqJLvs9RQDmhvBGt3l2Fzo/edit
+public class SqlBaseListener extends SQLGrammarBaseListener {
+    public SqlBaseListener() {
+
+    }
+}
