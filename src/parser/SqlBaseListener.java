7c8f2346aac322e20015d42d2097385d80297810
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-29 14:51:56 -0500

    Updated base listener

--- src/parser/SqlBaseListener.java
+++ src/parser/SqlBaseListener.java
@@ -7,14 +7,25 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 import parser.antlr.*;
 import dbms.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 // From following the "Guide - ANTLR4 Listener Setup" here: https://docs.google.com/document/d/1kCv5ODfAh8HZZ8uugMLcqMqJLvs9RQDmhvBGt3l2Fzo/edit
 public class SqlBaseListener extends SQLGrammarBaseListener {
     private Dbms dbms;
 
+    private Stack<String> relationNames; // Table names
+    private Stack<String> attributeNames; // Variable names, column names, etc
+    private Stack<Type> types; // Only used for CREATE
+    private Stack<Object> literals; // Can be strings or numbers
+
     public SqlBaseListener() {
         this.dbms = new Dbms();
+        relationNames = new Stack();
+        attributeNames = new Stack();
+        types = new Stack();
+        literals = new Stack();
     }
 
     @Override public void enterCommand(SQLGrammarParser.CommandContext ctx) { }
@@ -23,58 +34,78 @@ public class SqlBaseListener extends SQLGrammarBaseListener {
     @Override public void enterQuery(SQLGrammarParser.QueryContext ctx) { }
     @Override public void exitQuery(SQLGrammarParser.QueryContext ctx) { }
 
-    @Override public void enterCreate_cmd(SQLGrammarParser.Create_cmdContext ctx) {
-        List<ParseTree> children = ctx.children;
-
-        for(ParseTree child : children) {
-            System.out.println(child.getText());
+    // @Override public void enterCreate_cmd(SQLGrammarParser.Create_cmdContext ctx) { }
+    @Override public void exitCreate_cmd(SQLGrammarParser.Create_cmdContext ctx) {
+        String tableName = relationNames.pop();
+        List<String> primaryKeys = new ArrayList<>();
+        int primaryKeyCount = attributeNames.size() - types.size(); // How many primary keys there actually are
+        for(int i = 0; i < primaryKeyCount; i++) {
+            primaryKeys.add(attributeNames.pop());
         }
 
-        // String relation_name = children.get(0).getText();
+        List<String> columnNames = new ArrayList<>();
+        List<Type> columnTypes = new ArrayList<>();
+        while(types.empty()) { // Get the columns and their according types
+            columnNames.add(attributeNames.pop());
+            columnTypes.add(types.pop());
+        }
 
+        // Pass all of these values to Dbms
     }
-    @Override public void exitCreate_cmd(SQLGrammarParser.Create_cmdContext ctx) { }
-    @Override public void enterShow_cmd(SQLGrammarParser.Show_cmdContext ctx) { }
     @Override public void exitShow_cmd(SQLGrammarParser.Show_cmdContext ctx) { }
-    @Override public void enterUpdate_cmd(SQLGrammarParser.Update_cmdContext ctx) { }
     @Override public void exitUpdate_cmd(SQLGrammarParser.Update_cmdContext ctx) { }
-    @Override public void enterInsert_cmd(SQLGrammarParser.Insert_cmdContext ctx) { }
-    @Override public void exitInsert_cmd(SQLGrammarParser.Insert_cmdContext ctx) { }
-    @Override public void enterDelete_cmd(SQLGrammarParser.Delete_cmdContext ctx) { }
+    @Override public void exitInsert_cmd(SQLGrammarParser.Insert_cmdContext ctx) {
+        String tableToInsert = relationNames.pop();
+    }
     @Override public void exitDelete_cmd(SQLGrammarParser.Delete_cmdContext ctx) { }
 
     // Should return an integer
-    @Override public void enterInteger(SQLGrammarParser.IntegerContext ctx) { }
     @Override public void exitInteger(SQLGrammarParser.IntegerContext ctx) { }
 
     // Return a string
-    @Override public void enterIdentifier(SQLGrammarParser.IdentifierContext ctx) { }
     @Override public void exitIdentifier(SQLGrammarParser.IdentifierContext ctx) { }
-    @Override public void enterString_literal(SQLGrammarParser.String_literalContext ctx) { }
     @Override public void exitString_literal(SQLGrammarParser.String_literalContext ctx) { }
-    @Override public void enterLiteral(SQLGrammarParser.LiteralContext ctx) { }
     @Override public void exitLiteral(SQLGrammarParser.LiteralContext ctx) { }
 
-    // Probably return a String
-    @Override public void enterRelation_name(SQLGrammarParser.Relation_nameContext ctx) { }
-    @Override public void exitRelation_name(SQLGrammarParser.Relation_nameContext ctx) { }
-    @Override public void enterAttribute_name(SQLGrammarParser.Attribute_nameContext ctx) { }
-    @Override public void exitAttribute_name(SQLGrammarParser.Attribute_nameContext ctx) { }
+    @Override public void exitRelation_name(SQLGrammarParser.Relation_nameContext ctx) {
+        // What the hell do we do with relation name
+        System.out.println("Exit relation name");
+        String name = ctx.children.get(0).getText();
+        attributeNames.push(name);
+    }
+
+    @Override public void exitAttribute_name(SQLGrammarParser.Attribute_nameContext ctx) {
+        String name = ctx.children.get(0).getText();
+        relationNames.push(name);
+    }
 
     @Override public void enterOperand(SQLGrammarParser.OperandContext ctx) { }
     @Override public void exitOperand(SQLGrammarParser.OperandContext ctx) { }
 
     // Return a Type, either Varchar or Integer
-    @Override public void enterType(SQLGrammarParser.TypeContext ctx) { }
-    @Override public void exitType(SQLGrammarParser.TypeContext ctx) { }
+    @Override public void enterType(SQLGrammarParser.TypeContext ctx) {
+        // Check whether it's a varchar or integer
+        List<ParseTree> children = ctx.children;
+        String typeName = children.get(0).getText();
+        Type type;
 
-    // Return
-    @Override public void enterAttribute_list(SQLGrammarParser.Attribute_listContext ctx) { }
-    @Override public void exitAttribute_list(SQLGrammarParser.Attribute_listContext ctx) { }
+        if(typeName.equals("VARCHAR")) {
+            int size = Integer.parseInt(children.get(2).getText());
+
+            type = new Varchar(size);
+        } else { // Integer
+            type = new IntType();
+        }
+
+        types.push(type);
+    }
+    @Override public void exitType(SQLGrammarParser.TypeContext ctx) { }
 
     // Return a List<Type>
     @Override public void enterTyped_attribute_list(SQLGrammarParser.Typed_attribute_listContext ctx) { }
-    @Override public void exitTyped_attribute_list(SQLGrammarParser.Typed_attribute_listContext ctx) { }
+    @Override public void exitTyped_attribute_list(SQLGrammarParser.Typed_attribute_listContext ctx) {
+        // Get all of the relations & types, then get them as a list
+    }
 
     // Void?
     @Override public void enterOpen_cmd(SQLGrammarParser.Open_cmdContext ctx) { }
