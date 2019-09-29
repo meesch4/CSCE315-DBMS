ded73771da92d05fa47876e3724f86084946c98c
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-29 12:39:18 -0500

    Renamed Types to Type and made it an Abstract class

---
+++ src/dbms/Type.java
@@ -0,0 +1,7 @@
+package dbms;
+
+/** Contains all of the possible types to be used within the database
+ *  Used by Parser & Database when determining what type a column should be
+ */
+public abstract class Type {
+}
