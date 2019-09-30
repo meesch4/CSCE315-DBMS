df63694605572c61ac8913df71ed4c35b12468ae
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-29 20:47:52 -0500

    Updated tests for union, difference & product

--- tests/parser/SqlBaseListenerTests.java
+++ tests/parser/SqlBaseListenerTests.java
@@ -140,6 +140,21 @@ class FakeDbms implements IDbms {
     }
 
     @Override
+    public String union(String table1, String table2) {
+        return "tempTable" + count++;
+    }
+
+    @Override
+    public String difference(String table1, String table2) {
+        return "tempTable" + count++;
+    }
+
+    @Override
+    public String product(String table1, String table2) {
+        return "tempTable" + count++;
+    }
+
+    @Override
     public Table getTable(String tableName) {
         return null;
     }
