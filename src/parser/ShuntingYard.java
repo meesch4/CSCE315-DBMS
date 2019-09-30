ad4242bf44079de223dffc7c8502bb9517d87e27
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-29 22:19:26 -0500

    Added ShuntingYard w/ outline

---
+++ src/parser/ShuntingYard.java
@@ -0,0 +1,16 @@
+package parser;
+
+import dbms.Condition;
+
+/**
+ * Contains static methods that given a string, returns a Condition object
+ */
+public class ShuntingYard {
+    /**
+     *  Given an text input, returns a Condition object after performing the shunting-yard algorithm
+     *  Could also have a ParseTree object as a parameter rather than a String, which should save us on parsing
+     */
+    public static Condition evaulate(String input) { // Rename this?
+        return null;
+    }
+}
