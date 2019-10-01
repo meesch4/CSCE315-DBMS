54384a5eb17395a94d77a2192fc846de8a7da633
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-10-01 18:01:54 -0500

    Moved RowNode inside TableRowNode

--- src/dbms/TableRootNode.java
+++ src/dbms/TableRootNode.java
@@ -49,22 +49,24 @@ public class TableRootNode { //node containing relation name and attributes of t
         return this.attList;
     }
 
+    // If this gets to cluttered, move this to a separate file
+    public class RowNode {
+        RowNode(Object[] objects){ //can be used to pass a premade Object array to the class
+            dataFields = objects;
+        }
+        TableRootNode parent; // When would this be used?
+        // Removed the initialization as parent is going to be null when it's called
+        Object[] dataFields; // Contains all the VARCHARS/Integers in this row
+
+        public Object getDataField(int index){
+            return dataFields[index];
+        }
+        public void setDataField(int index, Object data){
+            dataFields[index] = data;
+        }
+        public String getRelation(){
+            return this.parent.relationName;
+        }
+    }
 }
 
-class RowNode {
-    RowNode(Object[] objects){ //can be used to pass a premade Object array to the class
-        dataFields = objects;
-    }
-    TableRootNode parent;
-    Object[] dataFields = new Object[this.parent.getAttributeSize()];
-    //this Object array contains all the VARCHARS and integers in the rows
-    public Object getDataField(int index){
-        return dataFields[index];
-    }
-    public void setDataField(int index, Object data){
-        dataFields[index] = data;
-    }
-    public String getRelation(){
-        return this.parent.relationName;
-    }
-}
