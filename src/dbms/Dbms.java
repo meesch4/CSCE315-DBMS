766e5c422872245199aea12a81f043fa1974e805
Trevor Turchan <tturchan1@gmail.com>
2019-10-04 17:52:02 -0500

    Updated insertFromRelation to support default values.
    It will now see if there are matching attributes in the two tables attributes list, map those attributes to the correct columns, or insert a default value if there is no matching attribute in the from table.

--- src/dbms/Dbms.java
+++ src/dbms/Dbms.java
@@ -61,24 +61,51 @@ public class Dbms implements IDbms {
             return;
         } */
 
-        // Find what attributes attListFrom is missing and add them to this list
-        List<Attribute> missingAttributes = new ArrayList<>();
+
+
+        // when an attribute in the attributes from list matches the name and type of an attribute in the attributes into list,
+        //this loop will create a tuple of the form [indexFrom, indexInto] to inform the next code block which columns in the
+        //FROM table to map to the which columns in the INTO table.
+        ArrayList<int[]> tuples = new ArrayList<>();
         for(Attribute attributeInto : attListInto) {
             boolean found = false;
             for(Attribute attributeFrom : attListFrom) {
-
+                if((attributeFrom.getType() == attributeInto.getType()) && (attributeFrom.getName() == attributeInto.getName())){
+                    int[] newTup = new int[]{attributeFrom.index, attributeInto.index};
+                    attListFrom.remove(attributeFrom);
+                    tuples.add(newTup);
+                    found = true;
+                    break;
+                }
             }
-
-            if(!found) {
-                missingAttributes.add(attributeInto);
+            if(!found){
+                int[] newTup = new int[]{-1, attributeInto.index};
+                tuples.add(newTup);
             }
         }
+        /*while(!(attListFrom.isEmpty())){
+            for(Attribute attributeFrom : attListFrom){
+                Object[] newTup = new Object[]{attributeFrom.index, null};
+                tuples.add(newTup);
+            }
+        }*/
+
+
 
         List<RowNode> rowListFrom = tableFrom.getRowNodes();
         for(RowNode rowFrom : rowListFrom){ //pulls each row node from table from
-            Object[] dataFieldsFrom = new Object[attListInto.size()];
-
-            tableInto.addRow(rowFrom);  //inserts them into table into
+            Object[] newDataFields = new Object[attListInto.size()];
+            for(int[] tuple : tuples){
+                int fromIndex = tuple[0];
+                int toIndex = tuple[1];
+                if(fromIndex == -1){ //if the value is not present in the from table
+                    newDataFields[toIndex] = null; //set value to null
+                }else{
+                    newDataFields[toIndex] = rowFrom.getDataField(fromIndex);
+                }
+            }
+            RowNode newRow = new RowNode(newDataFields);
+            tableInto.addRow(newRow);  //inserts them into table into
         }
     }
 
