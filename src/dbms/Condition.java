a127f07caa2d557d4c2f68c2f64bff56a731acc7
Trevor Turchan <tturchan1@gmail.com>
2019-10-03 21:41:40 -0500

    Fleshed out the comparisons for condition's evaluate function.
    I had to pass the TableRootNode in addition to the row being compared in order to make it work (I'm trash when it comes to handling private vs public classes.  I'm sure there's a better approach than this, but it should work, and it should be something that can be handled by only changing select and other methods that use evaluate, since these all have a string telling them which table they're taking data from).

--- src/dbms/Condition.java
+++ src/dbms/Condition.java
@@ -1,6 +1,7 @@
 package dbms;
 
 
+
 //to construct the conditions from your post fixed stack, you need a recursive function which
 //creates conditions, where you recurse if you have an && or an || operator.
 
@@ -9,6 +10,8 @@ package dbms;
 //If it's <= == etc, then those objects will be an attribute and value pair, and you don't need to recurse further.
 
 //If you hit the &&/|| then you will recurse further, and the next object will be another operator.
+
+import java.util.ArrayList;
 
 public class Condition {
     // Filled with table names, operators, and operands
@@ -20,31 +23,114 @@ public class Condition {
 
     //Syntax is garbage, and this needs to be properly implemented, but this explains the basic recursive structure of the evaluate funciton
     // I'll work through the specifics soon.
-    public boolean evaluate(Condition cond, RowNode row) {
+    public boolean evaluate(Condition cond, RowNode row, TableRootNode table) {
         Operator op = cond.op;
         Object value = null;
         Object literal = null;
+
         switch (op) {
             case AND:
-                return evaluate((Condition) cond.left, row) && evaluate((Condition) cond.right, row);
+                return evaluate((Condition) cond.left, row, table) && evaluate((Condition) cond.right, row, table);
             case OR:
-                return evaluate((Condition) cond.left, row) || evaluate((Condition) cond.right, row);
+                return evaluate((Condition) cond.left, row, table) || evaluate((Condition) cond.right, row, table);
+        }
+        //Split since the following for loop need not execute for the recursive evaluate calls
+        int indexToCompare;
+        int leftRight = -1;
+        for(int i = 0; i < table.getAttributeSize(); i++){
+            if(cond.left == table.getAttribute(i).attrName){
+                leftRight = 0;
+                indexToCompare = i;
+                i = table.getAttributeSize();
+            }else if(cond.right == table.getAttribute(i).attrName){
+                leftRight = 1;
+                indexToCompare = i;
+                i = table.getAttributeSize();
+            }
+        }
+        switch (op){
             case EQUALS:
                 // At this point, cond.left (Or cond.right) is going to be an attribute
                 // We need to find what index this attribute is from the table(we only have it's name at this point)
                 // then retrieve that corresponding value from the RowNode. Only then can we compare
+                for(int i = 0; i < table.getRowNodes().size(); i++){
+                    if(leftRight == 0){
+                        if(cond.right == row.getDataField(i)){
+                            return true;
+                        }
+                    }if(leftRight == 1) {
+                        if (cond.left == row.getDataField(i)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
 
-                return value.equals(literal);
             case NOT_EQUALS:
-                return !value.equals(literal);
+                for(int i = 0; i < table.getRowNodes().size(); i++){
+                    if(leftRight == 0){
+                        if(cond.right != row.getDataField(i)){
+                            return true;
+                        }
+                    }if(leftRight == 1) {
+                        if (cond.left != row.getDataField(i)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
             case LESS_EQ: // <=
-                return (int) value <= (int) literal;
+                for(int i = 0; i < table.getRowNodes().size(); i++){
+                    if(leftRight == 0){
+                        if((int) cond.right <= (int) row.getDataField(i)){
+                            return true;
+                        }
+                    }if(leftRight == 1) {
+                        if ((int) cond.left <= (int) row.getDataField(i)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
             case LESS:
-                return (int) value < (int) literal;
+                for(int i = 0; i < table.getRowNodes().size(); i++){
+                    if(leftRight == 0){
+                        if((int) cond.right < (int) row.getDataField(i)){
+                            return true;
+                        }
+                    }if(leftRight == 1) {
+                        if ((int) cond.left < (int) row.getDataField(i)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
             case GREATER_EQ:
-                return (int) value >= (int) literal;
+                for(int i = 0; i < table.getRowNodes().size(); i++){
+                    if(leftRight == 0){
+                        if((int) cond.right >= (int) row.getDataField(i)){
+                            return true;
+                        }
+                    }if(leftRight == 1) {
+                        if ((int) cond.left >= (int) row.getDataField(i)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
             case GREATER:
-                return (int) value > (int) literal;
+                for(int i = 0; i < table.getRowNodes().size(); i++){
+                    if(leftRight == 0){
+                        if((int) cond.right > (int) row.getDataField(i)){
+                            return true;
+                        }
+                    }if(leftRight == 1) {
+                        if ((int) cond.left > (int) row.getDataField(i)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
             default: break;
         }
 
