1a1bb364aa55ca72bd280c4502ca4cac4fe38ad8
Trevor Turchan <tturchan1@gmail.com>
2019-10-03 19:13:13 -0500

    Non-functioning evaluate function, but the condition object should be working.  I'll go through the logic to finalize evaluate, but the comments should give a good overview of how the condition object works, as well as how to construct it from your stack.

--- src/dbms/Condition.java
+++ src/dbms/Condition.java
@@ -1,11 +1,57 @@
 package dbms;
 
+
+//to construct the conditions from your post fixed stack, you need a recursive function which
+//creates conditions, where you recurse if you have an && or an || operator.
+
+//when you hit an operator, the next thing in the stack is going to be your right object
+//after that you'll have the left object.
+//If it's <= == etc, then those objects will be an attribute and value pair, and you don't need to recurse further.
+
+//If you hit the &&/|| then you will recurse further, and the next object will be another operator.
+
 public class Condition {
     // Filled with table names, operators, and operands
     // Might also contain Attributes
     // Will probably be the result of the shunting-yard algorithm
+    public Object left;
+    public Object right;
+    public Operator op;
 
-    public boolean evaluate() {
-        return false;
-    }
+
+
+
+//Syntax is garbage, and this needs to be properly implemented, but this explains the basic recursive structure of the evaluate funciton
+// I'll work through the specifics soon.
+
+
 }
+
+public class evaluate{
+    public boolean evaluate(Condition cond, RowNode row) {
+        Operator op = cond.op;
+        switch(op) {
+            case &&:
+                if (evaluate(cond.left, row) && evaluate(cond.right, row)) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            case ||:
+                if(evaluate(cond.left, row) || evaluate(cond.right, row)) {
+                    return true;
+                }else{
+                    return false;
+                }
+                break;
+            case <=:
+                if(cond.left <= cond.right) {
+                    return true;
+                }
+                else return false;
+                break;
+
+        }
+}
+
