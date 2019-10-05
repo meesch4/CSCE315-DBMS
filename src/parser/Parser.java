05202aea850474cbb19465839ca5a3c9ecc2a956
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-10-04 19:27:23 -0500

    Deleted Parser

--- src/parser/Parser.java
+++
@@ -1,9 +0,0 @@
-package parser;
-
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import parser.antlr.*;
-
-public class Parser {
-    public Parser() { }
-}
