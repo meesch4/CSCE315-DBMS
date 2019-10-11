62dec950d38b51f4664c6483c1bff3d6683222da
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-10-10 19:01:54 -0500

    Added outline for scraper.DataLoader class

--- src/scraper/DataLoader.java
+++ src/scraper/DataLoader.java
@@ -1,7 +1,19 @@
 package scraper;
 
-// Most likely rename this at some point
-// This is basically going to use the MovieDatabaseParser, convert the data points into tables,
-// then move those tables into Dbms
+//
+//
+//
+
+/**
+ * Most likely rename this at some point
+ * This is basically going to use the MovieDatabaseParser, convert the data points into tables,
+ * then move those tables into Dbms(or rather, save those tables to a file to be loaded later, using serialization)
+ */
 public class DataLoader {
+}
+
+// Won't actually be here, just for brainstorming of what we need
+interface IDataLoader {
+    // Will have a main that we only run once to do the scraping and convew
+    void main();
 }
