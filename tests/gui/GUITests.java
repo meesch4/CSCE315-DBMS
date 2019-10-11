11a4bdcba229cf00f4bde900a4fc18d4eb49a1af
meesch4 <meesch4@gmail.com>
2019-10-11 14:25:36 -0500

    removed simple errors. Issue with getting leave needs to be fixed.

---
+++ tests/gui/GUITests.java
@@ -0,0 +1,19 @@
+package gui;
+
+import org.junit.Test;
+import javax.swing.*;
+import types.IntType;
+import types.Type;
+import types.Varchar;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class GUITests {
+    // GUI output tests
+    // To be updated in the future to test query functions
+    @Test
+    public void basicGUITest() {}
+
+}
