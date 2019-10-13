77646096b501780d509a19c03974f00b995084ab
Trevor Turchan <tturchan1@gmail.com>
2019-10-13 16:19:42 -0500

    Pseudo code for CoverRoles created.
    Again, will be easy to implement once the SQL command generator is complete.  I'll need to fix the iteration through the hashmap of rows in the table, but other than that the semantic aspects should be correct.

--- src/query/interfaces/ICoverRolesQuery.java
+++ src/query/interfaces/ICoverRolesQuery.java
@@ -12,6 +12,22 @@ public interface ICoverRolesQuery {
      * Example
      * In: Joker
      * Out: Heath Ledger, Joaquin Phoenix, Jack Nicholson, Mark Hamil, Jared Leto, etc
+     *
+     * //create table of actor names where the credit has the proper character name using this sql command
+     * project (actorName) (select (characterName == inputName) credits) //also, actorName is the attribute for the actor name in the credits table, just to be clear.
+     *
+     * ArrayList<String>(TableRootNode tempTable){
+     *      Set<String> nameSet = new Set<>();
+     *      ArrayList<String> outputlist = new ArrayList<>();
+     *      for(each RowNode : tempTable.children){ //totally improper syntax, just putting this in as a place holder, before fixing the hashmap for each
+     *      //either way, this is supposed to iterate through all the string values for the actor names.
+     *          nameSet.add(rownode.value); //just to prevent duplicates.
+     *      }
+     *      for(each name : outputSet){
+     *          outputList.add(name);  //add to the output list
+     *      }
+     *      return outputList;  //this is the final list of actor names who have played the joker.  should be final return value of the query function.
+     * }
      */
     List<Credits.CastMember> calcActorsWhichPlayedCharacter(String characterName);
 }
