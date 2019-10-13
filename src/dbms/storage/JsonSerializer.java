a4a4d566e6bc979f2cd01558f29fcccfd3fbf8e7
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-10-13 15:54:16 -0500

    Implemented JsonSerializer

--- src/dbms/storage/JsonSerializer.java
+++ src/dbms/storage/JsonSerializer.java
@@ -2,25 +2,55 @@ package dbms.storage;
 
 import dbms.TableRootNode;
 
+import java.io.*;
 import java.util.HashMap;
 
 /**
  *  Saves and loads Tables to and from JSON files
  */
 public class JsonSerializer {
+    private static String filePath = "src/dbms/storage/";
+
     /**
      *  Saves the given table to a file, which is named according to its table name
      */
-    public void saveToFile(TableRootNode table) {
+    public static void saveToFile(TableRootNode table) {
+        String filePath = JsonSerializer.filePath + table.getRelationName() + ".ser";
 
+        try {
+            FileOutputStream fileOut = new FileOutputStream(filePath);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+
+            objectOut.writeObject(table);
+            objectOut.close();
+            fileOut.close();
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      *  Given a fileName(tableName), returns the according Table, or null if it couldn't be loaded
      */
-    public TableRootNode loadFromFile(String filePath) {
-        TableRootNode table = null;
+    public static TableRootNode loadFromFile(String tableName) {
+        String filePath = JsonSerializer.filePath + tableName + ".ser";
 
-        return table;
+        try {
+            FileInputStream fileIn = new FileInputStream(filePath);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+
+            TableRootNode table = (TableRootNode) objectIn.readObject();
+
+            objectIn.close();
+            fileIn.close();
+
+            return table;
+        } catch(Exception e) { // Catch ClassNotFound and IOException the same
+            e.printStackTrace();
+        }
+
+        // Failed, return null
+        return null;
     }
 }
