266d592eb0eacde1aaeecec62ad030b577d1fac6
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-10-13 15:44:10 -0500

    Renamed JsonSerializer methods

--- src/dbms/storage/JsonSerializer.java
+++ src/dbms/storage/JsonSerializer.java
@@ -11,14 +11,14 @@ public class JsonSerializer {
     /**
      *  Saves the given table to a file, which is named according to its table name
      */
-    public void serialize(TableRootNode table) {
+    public void saveToFile(TableRootNode table) {
 
     }
 
     /**
      *  Given a fileName(tableName), returns the according Table, or null if it couldn't be loaded
      */
-    public TableRootNode deserialze(String filePath) {
+    public TableRootNode loadFromFile(String filePath) {
         TableRootNode table = null;
 
         return table;
