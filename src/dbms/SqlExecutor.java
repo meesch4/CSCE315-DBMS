ccdb8ea5481708d6a851bb87264cd968a092dce9
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-10-13 18:29:36 -0500

    Added SQLExecutor

---
+++ src/dbms/SqlExecutor.java
@@ -0,0 +1,68 @@
+package dbms;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import parser.SqlBaseListener;
+import parser.antlr.SQLGrammarLexer;
+import parser.antlr.SQLGrammarParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SqlExecutor {
+    private final String sqlPath = "src/query/sql/";
+    private final String fileExtension = ".txt";
+
+    private Dbms db;
+
+    public SqlExecutor(Dbms db) {
+        this.db = db;
+    }
+
+    // Given a sql file, executes it and returns the table it returned, if any
+    public TableRootNode execute(String fileName) throws FileNotFoundException { // Where should we handle the exception?
+        SqlBaseListener listener = new SqlBaseListener(this.db);
+        List<String> lines = this.loadLines(fileName);
+
+        for(String line : lines) {
+            SQLGrammarLexer lexer = new SQLGrammarLexer(CharStreams.fromString(line));
+            SQLGrammarParser parser = new SQLGrammarParser(new CommonTokenStream(lexer));
+
+            lexer.removeErrorListeners();
+            parser.removeErrorListeners();
+
+            SQLGrammarParser.ProgramContext programContext = parser.program();
+            ParseTreeWalker walker = new ParseTreeWalker();
+
+            walker.walk(listener, programContext);
+        }
+
+        return db.getTable("RETURNED");
+    }
+
+    private List<String> loadLines(String fileName) throws FileNotFoundException {
+        List<String> lines = new ArrayList<>();
+
+        // String filePath = sqlPath + fileName + fileExtension;
+        String filePath = fileName;
+
+        File file = new File(filePath);
+        Scanner scanner = new Scanner(file);
+
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if(line.length() != 0) {
+                if(line.length() >= 2 && line.substring(0, 2).equals("//")) // Ignore commented lines
+                    continue;
+
+                lines.add(line);
+            }
+        }
+
+        return lines;
+    }
+}
