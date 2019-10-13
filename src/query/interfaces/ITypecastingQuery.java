345541b904bb62833ce5ac1c5bbb644bea79d831
Trevor Turchan <tturchan1@gmail.com>
2019-10-13 16:04:26 -0500

    Pseudo code for Typecasting Query created.
    Once we have a better grasp on how we're handling the SQL command creation, we can finish this up.
    I was a bit iffy on how the for each for the hash maps works, but that shouldn't impact the rest of the logic, and I was sure to comment a lot on what each line is supposed to be doing, so it shouldn't be hard to fix the syntax.

--- src/query/interfaces/ITypecastingQuery.java
+++ src/query/interfaces/ITypecastingQuery.java
@@ -5,6 +5,48 @@ import csce315.project1.*;
 public interface ITypecastingQuery {
     /**
      *  Given an actor's name, returns the most common genre the actor has played in
+     *
+     *
+     *  //project a table of genre values and movie ids from a table where we've selected all credits containing the input actor's name.
+     *
+     *  project (genre, movieId) (select (actorName == inputName) credits)
+     *
+     *
+     *  ///Pass this temporary table root node to a java function
+     *  String typeCast(TableRootNode genre){
+     *      HashMap<String, int> genreCount;
+     *      HashMap<String, int> movieIds;
+     *      int maxCount = 0;
+     *      String maxGenre = "";
+     *      //I definitely fucked up the for each, but that can be fixed.  but other than that, this should be correct.
+     *      for(genre.Children.Entry<String, RowNode> rowEntry : tableRows.entrySet()) { //iterate through each credit's genre list
+     *          String genreList = rowEntry.getValue(); //get the genre list as a string
+     *          String[] genres = genreList.split(","); //split the string by commas, to separate out each individual genre
+     *
+     *          ///////////////////////////////////////////////////////////////////
+     *          int MovieId = GET MOVIE ID FROM THE ROW ENTRY.  //need to fix this
+     *          movieIds.put(movieId, 0);
+     *          ///////////////////////////////////////////////////////////////////
+     *          if(!(movieIds.containsKey(movieId))){ //if the movie id has not already been checked ... i.e. this handles Tyler Perry
+     *          {
+     *              for(String genre : genres){ //for each of these genres, add them to the running count
+     *                  if(genreCount.containsKey(genre)){ //check if the genre count exists
+     *                      int count = genreCount.get(genre); /
+     *                      count++;
+     *                      genreCount.replace(genre, (count));  //add one to the counter for that genre.
+     *                      if(count > maxCount){
+     *                          maxCount = count;
+     *                          maxGenre = genre; //update the max genre
+     *                      }
+     *                  }else{
+     *                      genreCount.put(genre, 1); //put one as the count if the genre was not already in the genre count list.
+     *                  }
+     *              }
+     *          }
+     *      }
+     *      return maxGenre;
+     *
+     *  }
      */
     Movie.Genre calcMostCommonGenre(String actorName);
 }
