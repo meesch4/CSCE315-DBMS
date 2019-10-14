a31c13488144aefaa54a178cd3ebf9befa6a234f
Trevor Turchan <tturchan1@gmail.com>
2019-10-14 17:43:31 -0500

    Query for typecasting.  Incomplete.

--- src/query/Query.java
+++ src/query/Query.java
@@ -1,11 +1,7 @@
 package query;
 
-import dbms.Dbms;
-import dbms.TableRootNode;
+import dbms.*;
 import query.interfaces.*;
-import dbms.RowNode;
-import dbms.SqlExecutor;
-
 
 
 import java.util.*;
@@ -35,6 +31,47 @@ public class Query implements ICoverRolesQuery, IDegreeOfSeparationQuery, ITypec
              outputList.add(name);  //add to the output list
          }
          return outputList;  //this is the final list of actor names who have played the joker.  should be final return value of the query function.
+    }
+    String typeCast(TableRootNode genre) {
+        HashMap<String, int> genreCount;
+        HashMap<String, int> movieIds;
+        int maxCount = 0;
+        String maxGenre = "";
+        int genreIndex;
+        int movIdIndex;
+        for(Attribute att : genre.getAttributes()){
+            if(att.getName() == "movieID"){
+                movIdIndex = att.index;
+            }
+        }
+        //I definitely fucked up the for each, but that can be fixed.  but other than that, this should be correct.
+        for (Map.Entry<String, RowNode> rowEntry : genre.getRowNodes().entrySet()) { //iterate through each credit's genre list
+            RowNode row = rowEntry.getValue();
+            String genreList = (String) row.getDataField(0); //get the genre list as a string
+            String[] genres = genreList.split(","); //split the string by commas, to separate out each individual genre
+            ///////////////////////////////////////////////////////////////////
+            int MovieId = GET MOVIE ID FROM THE ROW ENTRY.  //need to fix this
+            movieIds.put(movieId, 0);
+            ///////////////////////////////////////////////////////////////////
+            if (!(movieIds.containsKey(movieId))) { //if the movie id has not already been checked ... i.e. this handles Tyler Perry
+                {
+                    for (String genre : genres) { //for each of these genres, add them to the running count
+                        if (genreCount.containsKey(genre)) { //check if the genre count exists
+                            int count = genreCount.get(genre); /
+                            count++;
+                            genreCount.replace(genre, (count));  //add one to the counter for that genre.
+                            if (count > maxCount) {
+                                maxCount = count;
+                                maxGenre = genre; //update the max genre
+                            }
+                        } else {
+                            genreCount.put(genre, 1); //put one as the count if the genre was not already in the genre count list.
+                        }
+                    }
+                }
+            }
+            return maxGenre;
+        }
     }
 
 
@@ -68,6 +105,18 @@ public class Query implements ICoverRolesQuery, IDegreeOfSeparationQuery, ITypec
     @Override
     public String calcMostCommonGenre(String actorName){
         String blah = "";
+
+           //Given an actor's name, returns the most common genre the actor has played in
+
+
+           //project a table of genre values and movie ids from a table where we've selected all credits containing the input actor's name.
+
+           //project (genre, movieId) (select (actorName == inputName) credits)
+
+
+           ///Pass this temporary table root node to a java function
+
+
         return blah;
     }
     @Override
